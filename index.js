cffeb0d1ca139bb66ef0440faf647d783bc0421a
pratyush8470 <pratyushbhatiya106@gmail.com>
2024-09-24 18:22:50 +0530

    cookie manage in admin pannel

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ const router = require('./router/index.js')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const db = require("./model/db/config");
+const passport = require('passport');
 
 
 app.set('view engine', 'ejs');
 app.set('views', Path);
 
+
+app.use(require('express-session')({ secret: 'secrethello', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(path.join(__dirname, "/public")));
