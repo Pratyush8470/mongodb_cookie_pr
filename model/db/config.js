0db82abf3c1bf89864a978b3941d4feed708cbd1
ankurchangani <changaniankur>
2024-09-24 14:02:39 +0530

    cookie pr

--- model/db/config.js
+++ model/db/config.js
@@ -6,12 +6,13 @@ mongoose.connect('mongodb+srv://pratyushbhatiya106:pratyush87888990@cluster1.2bd
         console.log('Could not connect to MongoDB', err);
     });
 
-const userSchema =  new  mongoose.Schema({
+const userSchema = new mongoose.Schema({
     fname: { type: String, require: true },
     lname: { type: String, require: true },
     email: { type: String, require: true },
     userName: { type: String, require: true },
-    password: { type: String, require: true }
+    password: { type: String, require: true },
+    path: { type: String, require: true }
 });
 
 const collection_d = mongoose.model('dbs', userSchema);
